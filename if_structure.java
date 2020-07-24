d8d77ae8713866c477671d14e9a303655f892725
jainsejal <sejaljain1194@gmail.com>
2020-07-24 19:38:35 +0200

    Add files via upload

---
+++ if_structure.java
@@ -0,0 +1,18 @@
+
+if (isValidPassword) {
+
+  System.out.println("Password accepted!");
+
+}
+
+// Prints "Password accepted!"
+
+int numberOfItemsInCart = 9;
+
+if (numberOfItemsInCart > 12) {
+
+  System.out.println("Express checkout not available");
+
+}
+
+// Nothing is printed.
